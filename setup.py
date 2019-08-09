755ce90717fcfb02aec2675bbb5c47ef67ce06b4
Ricardo Ribeiro <ricardojvr@gmail.com>
2019-08-09 12:04:57 +0100

    Update author email

--- setup.py
+++ setup.py
@@ -8,13 +8,17 @@ setup(
     version='0.0',
     description='',
     author='Ricardo Ribeiro',
-    author_email='ricardo.ribeiro@research.fchampalimaud.org',
+    author_email='ricardojvr@gmail.com',
     packages=find_packages(),
     install_requires=[
         'django-localflavor',
         'weasyprint',
         'django-weasyprint',
         'django-model-utils',
-        'django-money'
+        'django-money',
+        'core-common',
+        'core-finances',
+        'core-people',
+        'core-permissions',
     ]
 )
